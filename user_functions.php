a94fa0231af944bcaa475821f117b49917e1e088
Rom Levtov <romgamesrom@gmail.com>
2016-03-13 11:31:04 +0200

    Create user_functions.php

---
+++ user_functions.php
@@ -0,0 +1,4 @@
+<?
+	//TO-DO: Add function to get a name for the user, if username is taken, add a number at the end
+	//TO-DO (maybe?): Make so all server/user functions are made here and called from the 'server.php' file when a message is received.
+?>
